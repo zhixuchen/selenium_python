c9749f7fb04d2e94210d0b7c7b2f25ef5e58cc80
zxchen <1195733026@qq.com>
2020-02-05 10:04:46 +0800

    添加一键安装依赖模块

--- main/__init__.py
+++ main/__init__.py
@@ -3,5 +3,9 @@
 # author:zxchen
 # datetime:2018/10/16 11:30
 # software: PyCharm
+import os
 
+if __name__ == '__main__':
+    print(os.system('%s' % ("pip freeze > requirements.txt")))
 
+    print(os.system('%s' % ("pip install -r requirements.txt")))
