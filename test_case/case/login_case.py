94c04f446129a81722a6ecb12ba3cef48604cc69
zxchen <1195733026@qq.com>
2020-02-06 10:40:50 +0800

    优化tang

--- test_case/case/login_case.py
+++ test_case/case/login_case.py
@@ -12,78 +12,67 @@ class Login_Case(unittest.TestCase):
         self.driver = Driver()
         self.browser = self.driver.chrome_browser
         self.log=Logs()
+        self.data = Data()
 
     def login(self):
         try:
-            data = Data()
-            url = data.url
+            url = self.data.url
             self.browser.get(url)
         except Exception as e:
             print(e)
             catch_image(self.browser)
 
-    # def test_loginbypwd(self): # 密码登录
-    #     try:
-    #         data = Data()
-    #         account = data.get_account("xs_account")
-    #         # account = '18810000'
-    #         # pwd = data.get_pwd("xs_pwd")
-    #         pwd = data.get_pwd("xs_pwd")
-    #         find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account)
-    #         find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(pwd)
-    #         find_elements(self.browser, By.TAG_NAME, "button")[0].click()
-    #     except Exception as e:
-    #         print(e)
-    #         catch_image(self.browser)
-    #
-    # def test_loginbysms(self): # 验证码登录
-    #     try:
-    #         data = Data()
-    #         account = data.get_account("xs_account")
-    #         sms_code = data.sms_code
-    #         find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account) #账号输入框
-    #         find_elements(self.browser, By.TAG_NAME, "button")[1].click() # 登陆下方,登录方式切换按钮
-    #         find_elements(self.browser, By.TAG_NAME, "button")[0].click() # 获取验证码按钮
-    #         find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(sms_code) # 验证码输入框
-    #         find_elements(self.browser, By.TAG_NAME, "button")[1].click() # 登录
-    #     except Exception as  e:
-    #         print(e)
-    #         catch_image(self.browser)
-    #
-    # def test_loginsSuccess(self):
-    #     '''登录成功'''
-    #     self.login()
-    #     self.test_loginbypwd()
-    #     tip = find_element(self.browser, By.CLASS_NAME, "header-title").text
-    #     self.assertEqual(tip, '林润云系统')
+    def test_loginbypwd(self): # 密码登录
+        try:
+
+            account = self.data.get_account("xs_account")
+            pwd = self.data.get_pwd("xs_pwd")
+            find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account)
+            find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(pwd)
+            find_elements(self.browser, By.TAG_NAME, "button")[0].click()
+        except Exception as e:
+            print(e)
+            catch_image(self.browser)
+
+    def test_loginbysms(self): # 验证码登录
+        try:
+            account = self.data.get_account("xs_account")
+            sms_code = self.data.sms_code
+            find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account) #账号输入框
+            find_elements(self.browser, By.TAG_NAME, "button")[1].click() # 登陆下方,登录方式切换按钮
+            find_elements(self.browser, By.TAG_NAME, "button")[0].click() # 获取验证码按钮
+            find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(sms_code) # 验证码输入框
+            find_elements(self.browser, By.TAG_NAME, "button")[1].click() # 登录
+        except Exception as  e:
+            print(e)
+            catch_image(self.browser)
+
+    def test_loginsSuccess(self):
+        '''登录成功'''
+        self.login()
+        self.test_loginbypwd()
+        tip = find_element(self.browser, By.CLASS_NAME, "header-title").text
+        self.assertEqual(tip, '林润云系统')
 
     def test_nulluser(self):
         '''用户名为空-tang'''
         self.login()
-        # data = Data()
-        # account = data.get_account("xs_account")
-        account = '' # 用户名输入空,字符串
-        # pwd = data.get_pwd("xs_pwd")
-        # pwd = '123456'
-        find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account)
-        # find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(pwd)
+        pwd = self.data.get_pwd("xs_pwd")
+        find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(pwd)
         find_elements(self.browser, By.TAG_NAME, "button")[0].click()
-        time.sleep(1) # 提示不是瞬间弹出,需要时间响应,可用wait方法
-        tip = find_element(self.browser, By.XPATH, r"//*[@id='app']/div/div/div[2]/div/form/div/div[1]/div/div[2]").text
-        self.assertEqual(tip, '请输入手机号码')
+        time.sleep(1)
+        error_tip = find_element(self.browser, By.CLASS_NAME,"el-form-item__error").text
+        print("error_tip:"+error_tip)
+        self.assertEqual(error_tip, '请输入手机号码')
 
     def test_nullpwd(self):
         '''密码为空-tang'''
         self.login()
-        data = Data()
-        account = data.get_account("xs_account")
-        pwd = ''
+        account = self.data.get_account("xs_account")
         find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[0].send_keys(account)
-        # find_elements(self.browser, By.CLASS_NAME, "el-input__inner")[1].send_keys(pwd)
         find_elements(self.browser, By.TAG_NAME, "button")[0].click()
-        time.sleep(1) # 提示不是瞬间弹出,需要时间响应,可用wait方法
-        tip = find_element(self.browser, By.XPATH, r"//*[@id='app']/div/div/div[2]/div/form/div/div[2]/div/div[2]").text
-        self.assertEqual(tip, '密码不能为空')
+        error_tip = find_element(self.browser, By.CLASS_NAME, "el-form-item__error").text
+        self.assertEqual(error_tip, '密码不能为空')
 
     # def test_nulltxtVerify(self):
     #     '''验证码为空-huyx'''
@@ -101,7 +90,7 @@ class Login_Case(unittest.TestCase):
 
     def tearDown(self):
         self.log.log_info("登录测试结束")
-        time.sleep(1)
+        time.sleep(3)
 
         self.driver.tearDown()
 
